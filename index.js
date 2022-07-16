67d14df4a74a36264848159cc8c8556b06fec8c8
yunjaehyuk <db2011307@gmail.com>
2022-07-16 21:33:42 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const config = require('./config/key')
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://jaehyuk:listeri74@boilerplate.ihme1.mongodb.net/?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
   useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => console.log("MongoDB Connected..."))
   .catch(err => console.log(err))
+
+
+  
 app.get('/', (req, res) => {
-  res.send('Hello World! 안녕')
+  res.send('Hello World!')
 })
+
+app.post('/register', (req, res) =>{
+
+  // 회원 가입 할떄 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터 베이스에서 넣어준다.
+ 
+
+
+  const user = new User(req.body)
+
+  user.save((err, doc) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
